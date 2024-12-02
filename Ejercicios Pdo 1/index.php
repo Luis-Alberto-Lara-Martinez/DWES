e288f349d8be2192891abc61c814cbc4379a93f9
Luis Alberto <luisalbertolaramartinez3c@gmail.com>
2024-12-02 18:39:22 +0100

    Login acabado

---
+++ Ejercicios Pdo 1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+
+<body>
+    <h1>Aplicación de una base de datos</h1>
+    <a href="insertar.php">Insertar registros</a>
+    <br />
+    <a href="borrar.php">Borrar registros</a>
+    <br />
+    <a href="">sssssss</a>
+</body>
+
+</html>
--- Ejercicios Pdo 1/login.php
+++ Ejercicios Pdo 1/login.php
@@ -1,41 +1,75 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+include("funcionesConexion.php");
+if (isset($_POST["enviar"])) {
+    $existeUsuario = false;
+    $nombre = sanearDato($_POST["usuario"]);
+    $contrasena = sanearDato($_POST["contrasena"]);
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
+    $conexion = crearBD("Ciudadanos");
+    $conexion->query("CREATE TABLE Usuario (
+        nombre VARCHAR(50) PRIMARY KEY,
+        contrasena VARCHAR(50)
+    );");
+    $conexion->query("INSERT INTO Usuario VALUES ('root', '1234');");
 
-<body>
-    <h1>Autenticación</h1>
-    <form action="inicio.php" method="post">
-        <label for="usario">Usuario:
-            <input type="text" name="usuario" id="usuario">
-        </label>
-        <br />
-        <br />
-        <label for="contrasena">Contraseña
-            <input type="password" name="contrasena" id="contrasena">
-        </label>
-        <?php
-        include("funcionesConexion.php");
-        $conexion = crearBD("Ciudadanos");
-        $conexion->query("CREATE TABLE Usuario (
-            nombre VARCHAR(50) PRIMARY KEY,
-            contrasena VARCHAR(50)
-        );");
-        $conexion->query("INSERT INTO Usuario VALUES ('root', '1234');");
-        $resultado = $conexion->query("SELECT * FROM Usuario;");
-        $existeUsuario = false;
-        foreach ($resultado as $fila) {
-            ijofewjiorwijor
+    $resultado = $conexion->query("SELECT * FROM Usuario;");
+    foreach ($resultado as $fila) {
+        if ($fila["nombre"] == $nombre && $fila["contrasena"] == $contrasena) {
+            $existeUsuario = true;
         }
-        ?>
-        <br />
-        <br />
-        <input type="submit" value="Iniciar Sesión" name="enviar">
-    </form>
-</body>
+    }
 
-</html>
+    if ($existeUsuario) {
+        header("Refresh: 5; url=index.php");
+        exit("<h1>Cargando aplicación...</h1>");
+    } else {
+        cargarFormulario(true);
+    }
+} else {
+    cargarFormulario(false);
+}
+
+function sanearDato($valor)
+{
+    $valor = htmlspecialchars(strip_tags(trim($valor)), ENT_QUOTES, "utf-8");
+    return $valor;
+}
+
+function cargarFormulario($hayError)
+{
+    ?>
+    <!DOCTYPE html>
+    <html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+    </head>
+
+    <body>
+        <h1>Autenticación</h1>
+        <form action="login.php" method="post">
+            <label for="usario">Usuario:
+                <input type="text" name="usuario" id="usuario">
+            </label>
+            <br />
+            <br />
+            <label for="contrasena">Contraseña
+                <input type="password" name="contrasena" id="contrasena">
+            </label>
+            <?php
+            if ($hayError) {
+                echo "<p style='color: red'>Datos incorrectos</p>";
+            } else {
+                echo "<br /><br />";
+            }
+            ?>
+            <input type="submit" value="Iniciar Sesión" name="enviar">
+        </form>
+    </body>
+
+    </html>
+    <?php
+}
+?>
